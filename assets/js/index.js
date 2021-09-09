a4a08b9868d1ffbb541e61043d045e86b9d55b3f
Hasala2002 <hasala.h@hotmail.com>
2021-09-09 17:59:19 +0530

    added contact page

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+$('.likes-comments div:nth-child(1) ion-icon').click(()=>{
+    $('.likes-comments div:nth-child(1) ion-icon').toggleClass('liked')
+    $('.likes-comments div:nth-child(1) span').text('97')
+})
