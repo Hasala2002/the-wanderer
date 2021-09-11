53c615fa8ff74097f5fc6fc56cbb110b902d2d96
Hasala2002 <hasala.h@hotmail.com>
2021-09-11 10:50:15 +0530

    update blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,11 @@
+$(function() {
+    if($(window).scrollTop()>30){
+        $('.scroll-to-top-btn').removeClass('scroll-btn-hide')
+    }else{
+        $('.scroll-to-top-btn').addClass('scroll-btn-hide') 
+    }
+})
+
 $('.blog-textarea textarea').focusin(()=>{
     $('.blog-textarea button').css({
         "opacity":"1"
@@ -9,3 +17,15 @@ $('.blog-textarea textarea').focusout(()=>{
         "opacity":"0"
     })
 });
+
+$('.scroll-to-top-btn').click(()=>{
+    $(window).scrollTop(0);
+})
+
+$(window).scroll(()=>{
+    if($(window).scrollTop()>30){
+        $('.scroll-to-top-btn').removeClass('scroll-btn-hide')
+    }else{
+        $('.scroll-to-top-btn').addClass('scroll-btn-hide') 
+    }
+})
